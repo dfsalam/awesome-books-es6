5fa2aa99543b8e055b087cf5de655f320fc06450
dfsalam <dfsalam@gmail.com>
2023-01-17 10:29:12 -0500

    First commit

---
+++ modules/Book.js
@@ -0,0 +1,30 @@
+export default class Book {
+  constructor(title, author, id) {
+    this.title = title;
+    this.author = author;
+    if (id === undefined) {
+      this.createId();
+      return;
+    }
+    this.id = id;
+  }
+
+  createHtml () {
+    const book = document.createElement('div');
+    book.className = 'book';
+    book.innerHTML = `
+      <p>"<span>${this.title}</span>" by <span>${this.author}</span></p>
+      <button class="button--shadow" id="${this.id}">Remove</button>
+    `;
+    return book;
+  }
+
+  createId(tokenLen = 16) {
+    let id = '';
+    const chars = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789';
+    for (let i = 0; i < tokenLen; i += 1) {
+      id += chars.charAt(Math.floor(Math.random() * chars.length));
+    }
+    this.id = id;
+  }
+}
---
+++ modules/Books.js
@@ -0,0 +1,67 @@
+import Book from './Book.js';
+
+export default class Books {
+  constructor(container) {
+    this.books = JSON.parse(localStorage.getItem('books')) || [];
+    this.container = container;
+    this.paragraph = document.getElementById('list-p');
+    if (this.amount > 0) {
+      this.load();
+    }else{
+      this.noBooks();
+    }
+  }
+
+  noBooks() {    
+    if (this.amount===0){     
+      this.paragraph.classList.remove('d-none');
+      return;      
+    }
+    this.paragraph.classList.add('d-none');    
+  }
+
+  load() {
+    const instances = [];
+    const fragment = document.createDocumentFragment();
+    this.books.forEach(({ title, author, id }) => {
+      const book = new Book(title, author, id);
+      fragment.appendChild(this.create(book));
+      instances.push(book);
+    });
+    this.container.appendChild(fragment);
+    this.books = instances;
+  }
+
+  saveLocally() {
+    localStorage.setItem('books', JSON.stringify(this.books));
+  }
+
+  remove(button) {
+    this.books = this.books.filter((book) => book.id !== button.id);
+    button.parentElement.remove();
+    this.saveLocally();
+    //
+    this.noBooks();    
+  }
+
+  add(title, author) {
+    const book = new Book(title, author);
+    this.books.push(book);
+    this.container.appendChild(this.create(book));
+    this.saveLocally();
+    this.noBooks();    
+  }
+
+  create(book) {
+    const html = book.createHtml(this.remove);
+    const btn = html.querySelector('button');
+    btn.addEventListener('click', ({ target }) => {
+      this.remove(target);
+    });
+    return html;
+  }
+
+  get amount() {
+    return this.books.length;
+  }
+}
